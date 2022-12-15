e9be078619e3eeb77bfccb71a42f57a54c1bcb9f
Vincenzo M <vincenzomaiorana@gmail.com>
2022-12-15 15:47:36 +0100

    added js logic

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,22 @@
+const taskTemplate = (task, id) => `
+         <div class="task" id="task${id}" onclick="taskCompleted(${id})">
+            <span class="delete" onclick="deleteTask(${id})">❌</span><span 
+              id="titleTask${id}">${task}</span>
+         </div>
+`;
 
+const createNewTask = () => {
+  const taskList = document.getElementById("task-list");
+  const newTaskTitle = document.getElementById("newTask").value;
+  const newTaskID = taskList.children.length + 1;
+  taskList.innerHTML += taskTemplate(newTaskTitle, newTaskID);
+};
+
+const deleteTask = (id) => {
+  const taskList = document.getElementById("task-list");
+  const taskToRemove = document.getElementById(`task${id}`);
+  taskList.removeChild(taskToRemove);
+};
+const taskCompleted = (id) => {
+  document.getElementById(`titleTask${id}`).classList.toggle("completed");
+};
